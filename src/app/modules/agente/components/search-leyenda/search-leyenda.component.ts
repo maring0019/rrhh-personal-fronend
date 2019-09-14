5836f04e9bf69ba7005cde1b78dc0370464ec5a7
dnievas <nievas.david@gmail.com>
2019-09-14 13:42:39 -0300

    Ajustes estilos pagina de busqueda y listado de archivos

--- src/app/modules/agente/components/search-leyenda/search-leyenda.component.ts
+++ src/app/modules/agente/components/search-leyenda/search-leyenda.component.ts
@@ -8,14 +8,17 @@ import { Component, Input } from '@angular/core';
             <div class="d-flex flex-column text-center text-muted">
                 <span class="mdi mdi-arrow-up-bold-circle-outline mdi-48px"></span>
                 <h3><strong>{{ header }}</strong></h3>
-                <h5>{{ subHeader }}</h5>
+                <h5 *ngIf="!subheader">
+                    Podes refinar tu búsqueda a través del icono <span>"<plex-icon name="mdi mdi-filter-outline"></plex-icon>"</span> de filtrado
+                </h5>
+                <h5 *ngIf="subheader">{{ subHeader }}</h5>
             </div>
         </section>`,
   })
 
 export class SearchLeyendaComponent{
     @Input() header: string = 'Buscá un agente en la barra superior';
-    @Input() subHeader: string = 'Podes refinar tu búsqueda a través del botón "Búsqueda avanzada"';
+    @Input() subHeader: string;
 
     constructor() {}
 
