556a574591d3c9c739a27d3ca1b83e6d2f0e47aa
dnievas <nievas.david@gmail.com>
2020-02-12 16:32:55 -0300

    TM. ABM Guardia Periodos. Update/Create

--- src/app/modules/tm/components/guardia-periodos/create-update/form/create-update-form.component.ts
+++ src/app/modules/tm/components/guardia-periodos/create-update/form/create-update-form.component.ts
@@ -16,9 +16,9 @@ import { GuardiaPeriodo } from 'src/app/models/GuardiaPeriodos';
 
 export class GuardiaPeriodoCreateUpdateFormComponent  implements OnInit {
     
-    @Input() object:GuardiaPeriodo;
-    @Input() titulo:String = 'Titulo';
-    @Input() subtitulo:String = 'Subtitulo';
+    @Input() object: GuardiaPeriodo;
+    @Input() titulo: String = 'Titulo';
+    @Input() subtitulo: String = 'Subtitulo';
 
     @Output() cancel: EventEmitter<any> = new EventEmitter<any>();
     @Output() success: EventEmitter<any> = new EventEmitter<any>();
@@ -39,14 +39,25 @@ export class GuardiaPeriodoCreateUpdateFormComponent  implements OnInit {
 
     initForm(){
         return this.formBuilder.group({
+            _id                   : [ this.object._id ],
             fechaDesde            : [ this.object.fechaDesde ],
             fechaHasta            : [ this.object.fechaHasta ],
             nombre                : [ this.object.nombre ]
         });
     }
 
-    guardar(object):Observable<GuardiaPeriodo>{
-        return this.objectService.post(object);
+    /**
+     * Actualiza el campo nombre del formulario ante cualquier cambio de
+     * los campos fechaDesde o fechaHasta. Es una facilidad para evitarle
+     * al usuario ingresar un nombre arbitrario.
+     */
+    public updateNombre(){
+        let nombre = '';
+        const fd = this.form.value.fechaDesde;
+        const fh = this.form.value.fechaHasta;
+        if (fd) nombre += moment(fd).utc().format('MMMM/YYYY').toUpperCase();
+        if (fh) nombre += ' - ' + moment(fh).utc().format('MMMM/YYYY').toUpperCase();
+        this.form.patchValue( { nombre: nombre });
     }
 
     public cancelar(){
@@ -55,26 +66,41 @@ export class GuardiaPeriodoCreateUpdateFormComponent  implements OnInit {
     }
 
 
-    public invalid(){
-        if (this.form.invalid){
-            formUtils.markFormAsInvalid(this.form);
-            return true;
-        }
-        else {
-            return false;
-        }
+    private invalid(){
+        formUtils.markFormAsInvalid(this.form);
+        return this.form.invalid 
     }
 
-    public aceptar(){
+    public guardar(){
         if (this.invalid()) return;
         const object = this.form.value;
-        this.guardar(object)
+        if (this.object._id){
+            this.update(object);
+        }
+        else{
+            this.add(object);
+        }
+    }
+    
+    private update(object){
+        this.objectService.put(object)
             .subscribe(
                 data=> {
                     this.success.emit(data);
                     formUtils.resetForm(this.form, this._form);
                 },
                 error => this.error.emit(error)
-            )
-    } 
+            );
+    }
+
+    private add(object){
+        this.objectService.post(object)
+            .subscribe(
+                data=> {
+                    this.success.emit(data);
+                    formUtils.resetForm(this.form, this._form);
+                },
+                error => this.error.emit(error)
+            );
+    }
 }
--- src/app/modules/tm/components/guardia-periodos/create-update/guardia-periodo-create-update.component.ts
+++ src/app/modules/tm/components/guardia-periodos/create-update/guardia-periodo-create-update.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
 import { Plex } from '@andes/plex';
 
@@ -20,7 +20,6 @@ export class GuardiaPeriodoCreateUpdateComponent implements OnInit {
     
     constructor
     (
-        private router: Router,
         private route: ActivatedRoute,
         private location: Location,
         private plex: Plex,
