d6162fce52e2ed6b2f311cb12c17875d7a28d0a8
David Nievas <nievas.david@gmail.com>
2020-09-08 16:11:07 -0300

    Partes. Ajustes para recuperar correctamente los 'servicios' a los que tiene acceso un jefe partir de las ubicaciones

--- src/app/pages/partes/parte-agente/list/parte-agente-list.component.ts
+++ src/app/pages/partes/parte-agente/list/parte-agente-list.component.ts
@@ -1,75 +1,71 @@
-import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
-import { Plex } from '@andes/plex';
+import { Component, Input } from "@angular/core";
+import { Router } from "@angular/router";
+import { Plex } from "@andes/plex";
 
-import { ABMListComponent } from 'src/app/modules/tm/components/crud/abm-list.component';
+import { ABMListComponent } from "src/app/modules/tm/components/crud/abm-list.component";
 
-import { ObjectService } from 'src/app/services/tm/object.service';
-import { ParteService } from 'src/app/services/parte.service';
-import { Parte } from 'src/app/models/Parte';
-import { ParteAgente } from 'src/app/models/ParteAgente';
-import { UbicacionService } from 'src/app/services/ubicacion.service';
-import { ParteJustificacionService } from 'src/app/services/parte-justificacion.service';
-
+import { ObjectService } from "src/app/services/tm/object.service";
+import { ParteService } from "src/app/services/parte.service";
+import { Parte } from "src/app/models/Parte";
+import { ParteAgente } from "src/app/models/ParteAgente";
+import { UbicacionService } from "src/app/services/ubicacion.service";
+import { ParteJustificacionService } from "src/app/services/parte-justificacion.service";
 
 @Component({
-    selector: 'app-parte-agente-list',
-    templateUrl: './parte-agente-list.html',
+    selector: "app-parte-agente-list",
+    templateUrl: "./parte-agente-list.html",
 })
 export class ParteAgenteListComponent extends ABMListComponent {
-
     @Input() editionEnabled = false;
 
     public saveButtonEnabled = false;
     public estadoPresentacionConfirmada;
 
-    public parteToUpdate:Parte;
-    public partesAgentes:ParteAgente[] = [];
+    public parteToUpdate: Parte;
+    public partesAgentes: ParteAgente[] = [];
 
     public justificaciones = []; // Opciones para el select de cada parte
 
-    public modelName = 'parte-agente';
+    public modelName = "parte-agente";
 
     // list-head options
-    public columnDef =
-    [
+    public columnDef = [
         {
-            id: 'agente',
-            title: 'Agente',
-            size: '16'
+            id: "agente",
+            title: "Agente",
+            size: "16",
         },
         {
-            id: 'entrada',
-            title: 'Entrada',
-            size: '13'
-        }
-        ,
-        {
-            id: 'salida',
-            title: 'Salida',
-            size: '13'
+            id: "entrada",
+            title: "Entrada",
+            size: "13",
         },
         {
-            id: 'horas',
-            title: 'Hs. Trabajo',
-            size: '13'
+            id: "salida",
+            title: "Salida",
+            size: "13",
         },
         {
-            id: 'articulo',
-            title: 'Artículo',
-            size: '10'
+            id: "horas",
+            title: "Hs. Trabajo",
+            size: "13",
         },
         {
-            id: 'justificacion',
-            title: 'Justificación',
-            size: '20'
+            id: "articulo",
+            title: "Artículo",
+            size: "10",
         },
         {
-            id: 'observaciones',
-            title: 'Obs.',
-            size: '10'
-        }
-    ]
+            id: "justificacion",
+            title: "Justificación",
+            size: "20",
+        },
+        {
+            id: "observaciones",
+            title: "Obs.",
+            size: "10",
+        },
+    ];
 
     constructor(
         protected router: Router,
@@ -77,39 +73,42 @@ export class ParteAgenteListComponent extends ABMListComponent {
         private parteService: ParteService,
         private ubicacionService: UbicacionService,
         private parteJustificacionService: ParteJustificacionService,
-        public plex: Plex) {
-            super(router, objectService);
-         }
+        public plex: Plex
+    ) {
+        super(router, objectService);
+    }
 
-    protected get dataService(){
+    protected get dataService() {
         return this.parteService;
     }
 
-    public ngOnInit() {    
-        this.parteJustificacionService.get({})
-            .subscribe(data => {
-                this.justificaciones = data
-            })
+    public ngOnInit() {
+        this.parteJustificacionService.get({}).subscribe((data) => {
+            this.justificaciones = data;
+        });
     }
 
-    search(searchParams){
+    search(searchParams) {
         this.searchStart(searchParams);
-        this.objectService.get(this.dataService, {...searchParams,...this.sortParams})
-        .subscribe(
-            objects => {
-                return (objects && objects.length)? this.searchPartesAgentes(objects[0]):this.createPartes(searchParams);
-            },
-            (err) => {
-                this.searchPartesAgentes(null);
-            }
-        );
+        this.objectService
+            .get(this.dataService, { ...searchParams, ...this.sortParams })
+            .subscribe(
+                (objects) => {
+                    return objects && objects.length
+                        ? this.searchPartesAgentes(objects[0])
+                        : this.createPartes(searchParams);
+                },
+                (err) => {
+                    this.searchPartesAgentes(null);
+                }
+            );
     }
 
-    searchPartesAgentes(parte:Parte){
+    searchPartesAgentes(parte: Parte) {
         this.refreshParte(parte);
-        if (parte && parte._id){
+        if (parte && parte._id) {
             this.parteService.getPartesAgentes(parte._id).subscribe(
-                objects => {
+                (objects) => {
                     this.partesAgentes = objects;
                     this.searchEnd(objects);
                 },
@@ -117,50 +116,57 @@ export class ParteAgenteListComponent extends ABMListComponent {
                     this.searchEnd([]);
                 }
             );
-        }
-        else {
+        } else {
             this.searchEnd([]);
         }
-        
     }
 
-    createPartes(params){
-        if (!params['fecha'] || !params['ubicacion.codigo']) return this.searchPartesAgentes(null);
-        
-        this.ubicacionService.getByCodigo(params['ubicacion.codigo'])
-        .subscribe( ubi => {
-            if (ubi){
-                const ubicacion = { codigo: ubi.codigo, nombre: ubi.nombre }
-                let parte = new Parte({ fecha: params.fecha, ubicacion: ubicacion });
-                this.parteService.post(parte).subscribe(
-                    object => this.searchPartesAgentes(object) 
-                    ,(err) => {
-                        this.searchPartesAgentes(null);
-                    }
-                );
-            }
-        }),
-        (err) => {
-            this.searchPartesAgentes(null);
-        }            
+    createPartes(params) {
+        if (!params["fecha"] || !params["ubicacion.codigo"])
+            return this.searchPartesAgentes(null);
+        this.ubicacionService
+            .getByCodigo(params["ubicacion.codigo"])
+            .subscribe((ubi) => {
+                if (ubi) {
+                    const ubicacion = {
+                        codigo: ubi.codigo,
+                        nombre: ubi.nombre,
+                    };
+                    let parte = new Parte({
+                        fecha: params.fecha,
+                        ubicacion: ubicacion,
+                    });
+                    this.parteService.post(parte).subscribe(
+                        (object) => this.searchPartesAgentes(object),
+                        (err) => {
+                            this.searchPartesAgentes(null);
+                        }
+                    );
+                }
+            }),
+            (err) => {
+                this.searchPartesAgentes(null);
+            };
     }
 
-    public refreshParte(parte:Parte){
+    public refreshParte(parte: Parte) {
         this.parteToUpdate = parte;
-        if (parte && parte.estado && parte.estado.nombre == "Presentación total") {
+        if (
+            parte &&
+            parte.estado &&
+            parte.estado.nombre == "Presentación total"
+        ) {
             this.estadoPresentacionConfirmada = true;
             this.enableEdition(false);
-        }
-        else {
+        } else {
             this.estadoPresentacionConfirmada = false;
             this.enableEdition(true);
         }
     }
 
-    public onEnableEdition(){
+    public onEnableEdition() {
         this.enableEdition(true);
     }
-
 
     /**
      * Guardado 'temporal' sin confirmacion definitiva de todos los partes
@@ -169,64 +175,78 @@ export class ParteAgenteListComponent extends ABMListComponent {
      * El usuario puede continuar realizando cambios hasta confirmar el parte
      * pero los cambios no son notificados para su aprobacion/auditoria.
      */
-    public onSavePartes(){
-        this.parteService.guardar(this.parteToUpdate, this.items)
-            .subscribe(data => {
+    public onSavePartes() {
+        this.parteService
+            .guardar(this.parteToUpdate, this.items)
+            .subscribe((data) => {
                 // TODO Mejorar el manejo de errores
-                this.plex.info('info', `Parte actualizado correctamente. El parte
-                    se encuentra aún es estado de 'Presentación Parcial'`)
-                    .then( e => {
+                this.plex
+                    .info(
+                        "info",
+                        `Parte actualizado correctamente. El parte
+                    se encuentra aún es estado de 'Presentación Parcial'`
+                    )
+                    .then((e) => {
                         this.refreshParte(data);
-                });
-        })
+                    });
+            });
     }
-
 
     /**
      * Guardado con confirmacion definitiva de todos los partes de agentes
-     * editados en el listado de partes. El estado del parte queda como 
-     * Presentacion Total. 
-     * El usuario puede continuar realizando cambios sobre el parte pero 
+     * editados en el listado de partes. El estado del parte queda como
+     * Presentacion Total.
+     * El usuario puede continuar realizando cambios sobre el parte pero
      * debe editarlo en forma explicita y cada cambio es notificado para
      * su aprobacion o auditoria
      */
-    public onConfirmarPartes(){
-        this.parteService.confirmar(this.parteToUpdate, this.items)
-            .subscribe(data => {
+    public onConfirmarPartes() {
+        this.parteService
+            .confirmar(this.parteToUpdate, this.items)
+            .subscribe((data) => {
                 // TODO Mejorar el manejo de errores
-                this.plex.info('info', `Parte actualizado y confirmado correctamente.
-                    El parte se encuentra ahora en estado de 'Presentación Total'`)
-                    .then( e => {
+                this.plex
+                    .info(
+                        "info",
+                        `Parte actualizado y confirmado correctamente.
+                    El parte se encuentra ahora en estado de 'Presentación Total'`
+                    )
+                    .then((e) => {
                         this.refreshParte(data);
-                });
-        })
+                    });
+            });
     }
 
     /**
      * Guardado de un parte en edicion, que ya habia sido confirmado
-     * previamente. El estado del parte continua como Presentacion Total. 
+     * previamente. El estado del parte continua como Presentacion Total.
      * Se debe notificar a los responsables que corresponda de estos
      * cambios (responsabilidad del endpoint confirmar)
      */
-    public onSaveEdition(){
-        this.parteService.confirmar(this.parteToUpdate, this.items)
-        .subscribe(data => {
-            // TODO Mejorar el manejo de errores
-            // TODO Terminar de definir a quienes se notifica y que se notifica
-            this.plex.info('info', `Parte editado correctamente. Se ha notificado
-                a los responsables de esta actualización.`)
-                .then( e => {
-                    this.refreshParte(data);
+    public onSaveEdition() {
+        this.parteService
+            .confirmar(this.parteToUpdate, this.items)
+            .subscribe((data) => {
+                // TODO Mejorar el manejo de errores
+                // TODO Terminar de definir a quienes se notifica y que se notifica
+                this.plex
+                    .info(
+                        "info",
+                        `Parte editado correctamente. Se ha notificado
+                a los responsables de esta actualización.`
+                    )
+                    .then((e) => {
+                        this.refreshParte(data);
+                    });
             });
-        })
     }
 
-    public onCancelEdition(){
+    public onCancelEdition() {
         this.onCancel();
     }
 
-    public onCancel(){
-        this.router.navigate(['/partes']);
+    public onCancel() {
+        this.router.navigate(["/partes"]);
     }
 
     /**
@@ -236,7 +256,7 @@ export class ParteAgenteListComponent extends ABMListComponent {
      * parte y sus observaciones).
      * @param enable indica si se habiita o no la edicion
      */
-    private enableEdition(enable:boolean){
+    private enableEdition(enable: boolean) {
         this.editionEnabled = enable;
     }
 }
--- src/app/pages/partes/parte-agente/list/search/parte-agente-search.component.ts
+++ src/app/pages/partes/parte-agente/list/search/parte-agente-search.component.ts
@@ -1,85 +1,87 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { ABMSearchComponent } from 'src/app/modules/tm/components/crud/abm-search.component';
-import { Auth } from 'src/app/services/auth.service';
-
+import { Component, AfterViewInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+import { ABMSearchComponent } from "src/app/modules/tm/components/crud/abm-search.component";
+import { Auth } from "src/app/services/auth.service";
 
 @Component({
-    selector: 'app-parte-agente-search-form',
-    templateUrl: 'parte-agente-search.html',
+    selector: "app-parte-agente-search-form",
+    templateUrl: "parte-agente-search.html",
 })
-export class ParteAgenteSearchFormComponent extends ABMSearchComponent implements AfterViewInit {
-
+export class ParteAgenteSearchFormComponent
+    extends ABMSearchComponent
+    implements AfterViewInit {
     //Search form options
-    public servicioOpciones = this.authService.servicios;
+    public ubicacionOpciones = this.authService.servicios;
 
-    constructor(protected formBuilder: FormBuilder,
-            private authService: Auth) {
-        super(formBuilder)
+    constructor(protected formBuilder: FormBuilder, private authService: Auth) {
+        super(formBuilder);
     }
 
-    ngAfterViewInit(){
+    ngAfterViewInit() {
         // Parche para visualizar correctamente la fecha en el reactive form
         window.setTimeout(() => {
-            if (this.filterForm){
-                this.filterForm.patchValue({ 
-                    fecha: this.filterForm.value.fecha
-                    })
+            if (this.filterForm) {
+                this.filterForm.patchValue({
+                    fecha: this.filterForm.value.fecha,
+                });
                 this.search();
             }
         }, 1000);
     }
 
-
-    initFilterForm(){
+    initFilterForm() {
         this.filterForm = this.formBuilder.group({
-            fecha     : [ new Date()],
-            servicio  : [ this.servicioOpciones[0]]
+            fecha: [new Date()],
+            ubicacion: [
+                this.ubicacionOpciones.length
+                    ? this.ubicacionOpciones[0]
+                    : null,
+            ],
         });
     }
 
-    get searchFilterFormParameters(){
-        let params:any = {};
-        if (this.filterForm.valid){
+    get searchFilterFormParameters() {
+        let params: any = {};
+        if (this.filterForm.valid) {
             let form = this.filterForm.value;
-            if (form.fecha){
-                params['fecha'] = form.fecha;
+            if (form.fecha) {
+                params["fecha"] = form.fecha;
             }
-            if (form.servicio){ // Filtro por servicio del parte
-                params['ubicacion.codigo'] = form.servicio.ubicacion;
+            if (form.ubicacion) {
+                // Filtro por ubicacion del parte
+                params["ubicacion.codigo"] = form.ubicacion.codigo;
             }
         }
         return params;
     }
 
-
-//     /**
-//      * Simula un 'refresh' del listado de agentes del parte. Es de utilidad 
-//      * en casos en los que ya se ha generado previamente el listado, y luego
-//      * un agente o mas se hayan dado de alta o incorparado como agentes del 
-//      * servicio del parte y por lo tanto no figuren en el listado original. 
-//      */
-//     public reloadPartesAgentes(){
-//         if (!this.searchForm.valid) return ;
-//         let form = this.searchForm.value;
-//         this.agenteService.search({
-//             'situacionLaboral.cargo.servicio.ubicacion': form.servicio.ubicacion,
-//             'activo': true
-//         }).subscribe(agentes => {
-//             if (agentes.length){
-//                 let agentesAusentes = agentes.filter( x => !this.partesAgentes.filter( y => y.agente._id === x._id).length);
-//                 let partesAgentes:ParteAgente[] = []
-//                 for (const agente of agentesAusentes) {
-//                     const parteAgente = new ParteAgente({
-//                         parte: { _id: this.parte._id },
-//                         agente: { _id: agente._id, nombre: agente.nombre, apellido: agente.apellido },
-//                         fecha: this.parte.fecha
-//                     });
-//                     partesAgentes.push(parteAgente);
-//                 }
-//                 this.parteAgenteService.postMany(partesAgentes).subscribe();
-//                 this.buscar();
-//             }
-//         })
-//     }
+    //     /**
+    //      * Simula un 'refresh' del listado de agentes del parte. Es de utilidad
+    //      * en casos en los que ya se ha generado previamente el listado, y luego
+    //      * un agente o mas se hayan dado de alta o incorparado como agentes del
+    //      * servicio del parte y por lo tanto no figuren en el listado original.
+    //      */
+    //     public reloadPartesAgentes(){
+    //         if (!this.searchForm.valid) return ;
+    //         let form = this.searchForm.value;
+    //         this.agenteService.search({
+    //             'situacionLaboral.cargo.servicio.ubicacion': form.servicio.ubicacion,
+    //             'activo': true
+    //         }).subscribe(agentes => {
+    //             if (agentes.length){
+    //                 let agentesAusentes = agentes.filter( x => !this.partesAgentes.filter( y => y.agente._id === x._id).length);
+    //                 let partesAgentes:ParteAgente[] = []
+    //                 for (const agente of agentesAusentes) {
+    //                     const parteAgente = new ParteAgente({
+    //                         parte: { _id: this.parte._id },
+    //                         agente: { _id: agente._id, nombre: agente.nombre, apellido: agente.apellido },
+    //                         fecha: this.parte.fecha
+    //                     });
+    //                     partesAgentes.push(parteAgente);
+    //                 }
+    //                 this.parteAgenteService.postMany(partesAgentes).subscribe();
+    //                 this.buscar();
+    //             }
+    //         })
+    //     }
 }
