a02771c62dfc2245acd9703f719459fd301f560a
David Nievas <nievas.david@gmail.com>
2020-09-22 17:19:28 -0300

    Reportes. Modifica opciones de impresion estaticas y ahora las recupera desde un endpoint

--- src/app/pages/reportes/forms/reporte-seleccion-tipo.component.ts
+++ src/app/pages/reportes/forms/reporte-seleccion-tipo.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { ArticuloService } from 'src/app/services/articulo.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+
+import { ArticuloService } from "src/app/services/articulo.service";
+import { ReportesService } from "src/app/services/reportes.service";
 
 @Component({
-    selector: 'app-reporte-seleccion-tipo',
-    templateUrl: './reporte-seleccion-tipo.html',
+    selector: "app-reporte-seleccion-tipo",
+    templateUrl: "./reporte-seleccion-tipo.html",
 })
 export class ReporteSeleccionTipoComponent implements OnInit {
-
     public form: FormGroup;
 
     // Opciones de visualizacion
@@ -15,228 +16,202 @@ export class ReporteSeleccionTipoComponent implements OnInit {
     public showPeriodos;
     public showArticulos;
     public showAgrupamiento;
-    
+
     // Form select options
     public opcionesTiposReportes;
     public opcionesArticulos = [];
-    public opcionesAgrupamiento = [];
-    public opcionesOrdenamiento = [];
+    public $opcionesAgrupamiento = this.reportesService.getOpcionesAgrupamiento();
+    public $opcionesOrdenamiento = this.reportesService.getOpcionesOrdenamiento();
     public opcionesVisualizacion = [];
 
     constructor(
         private formBuilder: FormBuilder,
-        private articuloService: ArticuloService){}
-    
+        private articuloService: ArticuloService,
+        private reportesService: ReportesService
+    ) {}
+
     ngOnInit() {
         this.initFormSelectOptions();
         this.form = this.initForm();
     }
 
-    private initFormSelectOptions(){
+    private initFormSelectOptions() {
         this.opcionesTiposReportes = [
-            { id: 'listado_agentes', nombre: 'Listado de Agentes' },
-            { id: 'legajos_agentes', nombre: 'Legajos de Agentes' },
-            { id: 'ausentismo', nombre: 'Ausentismo' },
-            { id: 'ausentismo_totalesxarticulo', nombre: 'Ausentismo - Totales por artículo' },
-            { id: 'licencias_agentes', nombre: 'Licencias' }
-        ];
-        
-        this.opcionesOrdenamiento = [
-            { id: 'apellido', nombre: 'Apellido y Nombre' },
-            { id: 'numero', nombre: 'Número de Legajo' },
-            { id: 'documento', nombre: 'Documento' },
-            { id: 'estadoCivil', nombre: 'Estado Civil' },
-            { id: 'nacionalidad.nombre', nombre: 'Nacionalidad' },
-            { id: 'sexo', nombre: 'Sexo' },
-            // Domicilio
-            { id: 'direccion.localidad.nombre', nombre: 'Localidad' },
-            { id: 'direccion.localidad.provincia.nombre', nombre: 'Provincia' },
-            // Cargo TODO Terminar de cargar las opciones de ordenamiento
-            { id: 'situacionLaboral.cargo.sector.nombre', nombre: 'Lugar de Trabajo' },
-            // { id: 9, nombre: 'Servicio' },
-            // { id: 10, nombre: 'Departamento' },
-            // { id: 10, nombre: 'Norma Legal' },
-            // { id: 10, nombre: 'Categoría' },
-            // { id: 10, nombre: 'Función' },
+            { id: "listado_agentes", nombre: "Listado de Agentes" },
+            { id: "legajos_agentes", nombre: "Legajos de Agentes" },
+            { id: "ausentismo", nombre: "Ausentismo" },
+            {
+                id: "ausentismo_totalesxarticulo",
+                nombre: "Ausentismo - Totales por artículo",
+            },
+            { id: "licencias_agentes", nombre: "Licencias" },
         ];
 
-        this.opcionesAgrupamiento = [
-            { id: 'estadoCivil', nombre: 'Estado Civil' },
-            { id: 'nacionalidad.nombre', nombre: 'nacionalidad' },
-            { id: 'sexo', nombre: 'Sexo' },
-            // { id: 'sexo', nombre: 'Edad' }, TODO Ver como resolver esta opcion
-            // Domicilio
-            { id: 'direccion.localidad.nombre', nombre: 'Localidad' },
-            { id: 'direccion.localidad.provincia.nombre', nombre: 'Provincia' },
-            // Cargo TODO Implementar todas las opciones
-            { id: 'situacionLaboral.cargo.sector.nombre', nombre: 'Lugar de Trabajo' },
-            // { id: 9, nombre: 'Servicio' },
-            // { id: 10, nombre: 'Departamento' },
-            // { id: 10, nombre: 'Norma Legal' },
-            // { id: 10, nombre: 'Categoría' },
-            // { id: 10, nombre: 'Función' },
-        ];
-        this.articuloService.get({})
-            .subscribe(data => {
+        this.articuloService.get({}).subscribe((data) => {
             this.opcionesArticulos = data;
         });
     }
 
-    initForm()
-    {
+    initForm() {
         return this.formBuilder.group({
-            reporte          : [{ id: 'legajos_agentes', nombre: 'Legajos de Agentes' }],
-            agrupamiento     : [{ id: 'situacionLaboral.cargo.sector.nombre', nombre: 'Lugar de Trabajo' }],
-            ordenamiento     : [{ id: 'numero', nombre: 'Número de Legajo' },],
-            fechaDesde       : [],
-            fechaHasta       : [],
-            anioDesde        : [],
-            anioHasta        : [],
-            articulos        : [],
-            visualizacion    : []
+            reporte: [{ id: "legajos_agentes", nombre: "Legajos de Agentes" }],
+            agrupamiento: [
+                {
+                    id: "situacionLaboral.cargo.sector.nombre",
+                    nombre: "Lugar de Trabajo",
+                },
+            ],
+            ordenamiento: [{ id: "numero", nombre: "Número de Legajo" }],
+            fechaDesde: [],
+            fechaHasta: [],
+            anioDesde: [],
+            anioHasta: [],
+            articulos: [],
+            visualizacion: [],
         });
     }
 
-    public onChangeTipoReporte(e){
+    public onChangeTipoReporte(e) {
         // Debemos actualizar las opciones de filtrado en el form
         if (!e.value) return;
-        switch(e.value.id){
-            case 'legajos_agentes':
+        switch (e.value.id) {
+            case "legajos_agentes":
                 this.showAnios = false;
                 this.showPeriodos = false;
                 this.showArticulos = false;
                 this.showAgrupamiento = false;
                 break;
-            case 'listado_agentes':
+            case "listado_agentes":
                 this.showAnios = false;
                 this.showPeriodos = false;
                 this.showArticulos = false;
                 this.showAgrupamiento = true;
                 break;
-            case 'ausentismo':
+            case "ausentismo":
                 this.showAnios = false;
                 this.showPeriodos = true;
                 this.showArticulos = true;
                 this.showAgrupamiento = true;
                 break;
-            case 'ausentismo_totalesxarticulo':
+            case "ausentismo_totalesxarticulo":
                 this.showAnios = false;
                 this.showPeriodos = true;
                 this.showArticulos = true;
                 this.showAgrupamiento = true;
                 break;
-            case 'licencias_agentes':
+            case "licencias_agentes":
                 this.showAnios = true;
                 this.showPeriodos = false;
                 this.showArticulos = false;
                 this.showAgrupamiento = true;
-                break;  
+                break;
         }
     }
 
-    prepareSearchParams(){
-        let params:any = {}
+    prepareSearchParams() {
+        let params: any = {};
         let form = this.form.value;
         // Filters
-        if (form.reporte){
-            switch(form.reporte.id){
-                case 'legajos_agentes':
+        if (form.reporte) {
+            switch (form.reporte.id) {
+                case "legajos_agentes":
                     params = this.prepareLegajoSearchParams();
                     break;
-                case 'listado_agentes':
+                case "listado_agentes":
                     params = this.prepareListadoSearchParams();
                     break;
-                case 'ausentismo':
+                case "ausentismo":
                     params = this.prepareAusentismoSearchParams();
                     break;
-                case 'ausentismo_totalesxarticulo':
+                case "ausentismo_totalesxarticulo":
                     params = this.prepareAusentismoSearchParams();
                     break;
-                case 'licencias_agentes':
+                case "licencias_agentes":
                     params = this.prepareLicenciasSearchParams();
-                    break;  
+                    break;
             }
-            
         }
         return params;
     }
 
-    prepareLegajoSearchParams(){
-        let params:any = {};
+    prepareLegajoSearchParams() {
+        let params: any = {};
         let form = this.form.value;
         // Sorting
-        if (form.ordenamiento){
-            params['sort'] = form.ordenamiento.id;
+        if (form.ordenamiento) {
+            params["sort"] = form.ordenamiento.id;
         }
         return params;
     }
 
-    prepareListadoSearchParams(){
-        let params:any = {};
+    prepareListadoSearchParams() {
+        let params: any = {};
         let form = this.form.value;
         // Agrupamiento
-        if (form.agrupamiento){
-            params['$group'] = form.agrupamiento.id;
+        if (form.agrupamiento) {
+            params["$group"] = form.agrupamiento.id;
         }
         // Sorting
-        if (form.ordenamiento){
-            params['sort'] = form.ordenamiento.id;
+        if (form.ordenamiento) {
+            params["sort"] = form.ordenamiento.id;
         }
         return params;
     }
 
-    prepareAusentismoSearchParams(){
-        let params:any = {};
+    prepareAusentismoSearchParams() {
+        let params: any = {};
         let form = this.form.value;
-        if (form.fechaDesde){
-            params['fechaDesde'] = this.parseDate(form.fechaDesde);
+        if (form.fechaDesde) {
+            params["fechaDesde"] = this.parseDate(form.fechaDesde);
         }
-        if (form.fechaHasta){
-            params['fechaHasta'] = this.parseDate(form.fechaHasta);
+        if (form.fechaHasta) {
+            params["fechaHasta"] = this.parseDate(form.fechaHasta);
         }
-        if (form.articulos){
-            params['articulos'] =  (form.articulos.map(e=>e._id)).join();
+        if (form.articulos) {
+            params["articulos"] = form.articulos.map((e) => e._id).join();
         }
         // Agrupamiento
-        if (form.agrupamiento){
-            params['$group'] = form.agrupamiento.id;
+        if (form.agrupamiento) {
+            params["$group"] = form.agrupamiento.id;
         }
         // Sorting
-        if (form.ordenamiento){
-            params['sort'] = form.ordenamiento.id;
+        if (form.ordenamiento) {
+            params["sort"] = form.ordenamiento.id;
         }
         return params;
     }
 
-    private prepareLicenciasSearchParams(){
-        let params:any = {};
+    private prepareLicenciasSearchParams() {
+        let params: any = {};
         let form = this.form.value;
-        if (form.anioDesde && form.anioHasta){
-            params['anios'] = (this.parseAnios(form.anioDesde, form.anioHasta)).join();
+        if (form.anioDesde && form.anioHasta) {
+            params["anios"] = this.parseAnios(
+                form.anioDesde,
+                form.anioHasta
+            ).join();
         }
         // Agrupamiento
-        if (form.agrupamiento){
-            params['$group'] = form.agrupamiento.id;
+        if (form.agrupamiento) {
+            params["$group"] = form.agrupamiento.id;
         }
         // Sorting
-        if (form.ordenamiento){
-            params['sort'] = form.ordenamiento.id;
+        if (form.ordenamiento) {
+            params["sort"] = form.ordenamiento.id;
         }
         return params;
     }
 
     // TODO Implementar en otro lugar
-    private parseDate(date){
-        return moment(date).format('YYYY-MM-DD');
+    private parseDate(date) {
+        return moment(date).format("YYYY-MM-DD");
     }
 
-    private parseAnios(desde, hasta){
+    private parseAnios(desde, hasta) {
         let anios = [];
-        while (desde<=hasta) {
+        while (desde <= hasta) {
             anios.push(desde);
             desde = desde + 1;
         }
         return anios;
     }
-
 }
--- src/app/services/reportes.service.ts
+++ src/app/services/reportes.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
-import { Http, ResponseContentType, RequestOptions, URLSearchParams, Headers } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { Injectable } from "@angular/core";
+import {
+    Http,
+    ResponseContentType,
+    RequestOptions,
+    URLSearchParams,
+    Headers,
+} from "@angular/http";
+import { Observable } from "rxjs/Observable";
 
-import { Server } from '@andes/shared';
-import { environment } from 'src/environments/environment';
+import { Server } from "@andes/shared";
+import { environment } from "src/environments/environment";
 
 export interface Options {
     params?: any;
@@ -13,55 +19,55 @@ export interface Options {
 
 @Injectable()
 export class ReportesService {
-
-    private serverUrl= environment.API;
-    private baseUrl = '/modules/reportes'; // URL to web api
+    private serverUrl = environment.API;
+    private baseUrl = "/modules/reportes"; // URL to web api
 
     // Listado de reportes con su respectiva url
     private reportesUrl = {
-            // Reportes Generales
-            'listado_agentes': `${this.serverUrl}${this.baseUrl}/agentes/listado`,
-            'legajos_agentes': `${this.serverUrl}${this.baseUrl}/agentes/legajo`,
-            'ausentismo': `${this.serverUrl}${this.baseUrl}/agentes/ausentismo`,
-            'ausentismo_totalesxarticulo': `${this.serverUrl}${this.baseUrl}/agentes/ausentismo/totalesporarticulo`,
-            'licencias_agentes': `${this.serverUrl}${this.baseUrl}/agentes/licencias`,
-            // Partes
-            'partes_agentes': `${this.serverUrl}${this.baseUrl}/agentes/partes`,
-            // Ausentismo
-            'ausentismo_certificado': `${this.serverUrl}${this.baseUrl}/ausentismo/certificado`,
-            // Agentes
-            'agentes_credencial': `${this.serverUrl}${this.baseUrl}/agentes/credencial`,
-        }
-    
-    constructor(private server: Server, private http: Http) { }
+        // Reportes Generales
+        listado_agentes: `${this.serverUrl}${this.baseUrl}/agentes/listado`,
+        legajos_agentes: `${this.serverUrl}${this.baseUrl}/agentes/legajo`,
+        ausentismo: `${this.serverUrl}${this.baseUrl}/agentes/ausentismo`,
+        ausentismo_totalesxarticulo: `${this.serverUrl}${this.baseUrl}/agentes/ausentismo/totalesporarticulo`,
+        licencias_agentes: `${this.serverUrl}${this.baseUrl}/agentes/licencias`,
+        // Partes
+        partes_agentes: `${this.serverUrl}${this.baseUrl}/agentes/partes`,
+        // Ausentismo
+        ausentismo_certificado: `${this.serverUrl}${this.baseUrl}/ausentismo/certificado`,
+        // Agentes
+        agentes_credencial: `${this.serverUrl}${this.baseUrl}/agentes/credencial`,
+    };
 
-    public download(tipoReporte:string, params?: any): Observable<any> { 
+    constructor(private server: Server, private http: Http) {}
+
+    public download(tipoReporte: string, params?: any): Observable<any> {
         const url = this.reportesUrl[tipoReporte];
         let options = this.prepareOptions({ params: params, showError: true });
         options.responseType = ResponseContentType.Blob;
         // const options = new RequestOptions({responseType: ResponseContentType.Blob });
-        return this.http.get(url+'/download', options)
-            .map(res => {
+        return this.http
+            .get(url + "/download", options)
+            .map((res) => {
                 return {
-                    filename: tipoReporte+'.pdf',
-                    file: res.blob()
+                    filename: tipoReporte + ".pdf",
+                    file: res.blob(),
                 };
             })
-            .catch(this.handleError)
+            .catch(this.handleError);
     }
 
-    public show(tipoReporte:string, params?: any): Observable<any> { 
+    public show(tipoReporte: string, params?: any): Observable<any> {
         const url = this.reportesUrl[tipoReporte];
         let options = this.prepareOptions({ params: params, showError: true });
         options.responseType = ResponseContentType.Text;
         return this.http.get(url, options).catch(this.handleError);
     }
 
-    public descargarArchivo(data){
+    public descargarArchivo(data) {
         let url = window.URL.createObjectURL(data.file);
-        let a = document.createElement('a');
+        let a = document.createElement("a");
         document.body.appendChild(a);
-        a.setAttribute('style', 'display: none');
+        a.setAttribute("style", "display: none");
         a.href = url;
         a.download = data.filename;
         a.click();
@@ -69,17 +75,32 @@ export class ReportesService {
         a.remove();
     }
 
+    public getOpcionesAgrupamiento() {
+        let url = `${this.baseUrl}/opcionesAgrupamiento`;
+        return this.server.get(url, { showLoader: false });
+    }
+
+    public getOpcionesOrdenamiento() {
+        let url = `${this.baseUrl}/opcionesOrdenamiento`;
+        return this.server.get(url, { showLoader: false });
+    }
+
     private handleError(error: any) {
-        let errMsg = (error.message) ? error.message :
-            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        let errMsg = error.message
+            ? error.message
+            : error.status
+            ? `${error.status} - ${error.statusText}`
+            : "Server error";
         return Observable.throw(errMsg);
     }
 
     private prepareOptions(options: Options): RequestOptions {
         let result = new RequestOptions({
             headers: new Headers({
-                'Content-Type': 'application/json',
-                'Authorization': window.sessionStorage.getItem('jwt') ? 'JWT ' + window.sessionStorage.getItem('jwt') : null
+                "Content-Type": "application/json",
+                Authorization: window.sessionStorage.getItem("jwt")
+                    ? "JWT " + window.sessionStorage.getItem("jwt")
+                    : null,
             }),
         });
         if (options && options.params) {
@@ -87,12 +108,17 @@ export class ReportesService {
             for (let param in options.params) {
                 if (options.params[param] !== undefined) {
                     if (Array.isArray(options.params[param])) {
-                        (options.params[param] as Array<any>).forEach((value) => {
-                            result.search.append(param, value);
-                        });
+                        (options.params[param] as Array<any>).forEach(
+                            (value) => {
+                                result.search.append(param, value);
+                            }
+                        );
                     } else {
                         if (options.params[param] instanceof Date) {
-                            result.search.set(param, (options.params[param] as Date).toISOString());
+                            result.search.set(
+                                param,
+                                (options.params[param] as Date).toISOString()
+                            );
                         } else {
                             result.search.set(param, options.params[param]);
                         }
