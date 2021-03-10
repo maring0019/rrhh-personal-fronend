5a33a51b08831844460ae637f79e0aa70f645538
David Nievas <nievas.david@gmail.com>
2021-03-10 00:51:29 -0300

    Partes. Reporte de Ingresos y Egresos. Reporte Diarios por Agente.  Se limitan los agentes al servicio del jefe de servicio

--- src/app/pages/partes/reportes/partes/search/parte-reporte-search.component.ts
+++ src/app/pages/partes/reportes/partes/search/parte-reporte-search.component.ts
@@ -1,16 +1,32 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ABMSearchComponent } from 'src/app/modules/tm/components/crud/abm-search.component';
+import { Auth } from 'src/app/services/auth.service';
 
 
 @Component({
     selector: 'app-parte-reporte-search-form',
     templateUrl: 'parte-reporte-search.html',
 })
-export class ParteReporteSearchFormComponent extends ABMSearchComponent implements AfterViewInit {
+export class ParteReporteSearchFormComponent extends ABMSearchComponent implements OnInit, AfterViewInit {
 
-    constructor(protected formBuilder: FormBuilder) {
+    // Permisos especiales. Si el usuario logueado dispone de este permiso podra
+    // consultar libremente en el modulo. Caso contrario estará restringido a los 
+    // servicios disponibles como jefe de servicio/dpto/etc.    
+    public canProcesarParte:Boolean = false;
+    
+    // Id de los servicios del jefe de servicio / servicio alias ubicacion
+    public serviciosAllowed;
+
+    constructor(protected formBuilder: FormBuilder,
+                private authService: Auth) {
         super(formBuilder)
+    }
+
+    async ngOnInit() {
+        this.canProcesarParte = await this.authService.check('partes:parte:procesar_parte');
+        this.serviciosAllowed = this.getServiciosAllowed();
+        this.initFilterForm(); 
     }
 
     ngAfterViewInit(){
@@ -51,4 +67,18 @@ export class ParteReporteSearchFormComponent extends ABMSearchComponent implemen
         }
         return params;
     }
+
+     /**
+     * Si el usuario tiene otorgado el permiso para procesar partes, entonces
+     * esta autorizado a visualizar todas los partes de todos los servicios.
+     * Caso contrario solo puede visualizar los partes de su servicio/s. Este
+     * metodo debe ser llamado luego de haber determinado si el usuario puede
+     * procesar un parte.
+     */
+    getServiciosAllowed(){
+        if (this.serviciosAllowed) return this.serviciosAllowed;
+        
+        return (!this.canProcesarParte)? this.authService.servicios: [];
+    }
+    
 }
