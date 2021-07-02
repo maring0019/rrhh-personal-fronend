f717eb1879f64469f1b4ef242d6abd1b7860dcc7
Mariano Andres Botta <marianoabotta@gmail.com>
2021-07-02 08:28:40 -0300

    fix: paginado borraba un elemento

--- src/app/modules/tm/components/crud/abm-list.component.ts
+++ src/app/modules/tm/components/crud/abm-list.component.ts
@@ -6,22 +6,22 @@ import { ObjectService } from 'src/app/services/tm/object.service';
 export abstract class ABMListComponent implements OnInit {
 
     public modelName = 'articulos';
-    public canCreateObject: boolean = true;
+    public canCreateObject = true;
 
-    public items:any[] = [];           // Contenedor de objetos visibles para el listado
+    public items: any[] = [];           // Contenedor de objetos visibles para el listado
     public itemSelected: any;          // Objeto seleccionado del listado
-    private hiddenItems:any[];         // Contenedor de todos los objetos consultados
-    
+    private hiddenItems: any[];         // Contenedor de todos los objetos consultados
 
-    // Variable de control para el proceso de busqueda  
+
+    // Variable de control para el proceso de busqueda
     public searchParams = {}; // To keep a local copy of searched params
-    public searching = false;      
+    public searching = false;
     public searched = false;
     public showMore: Boolean = false;
 
     // list-head options
-    public columnDef:any = [];
-    public sortColumn:string;
+    public columnDef: any = [];
+    public sortColumn: string;
 
     protected dataService: any;          // Service with http methods
 
@@ -30,127 +30,126 @@ export abstract class ABMListComponent implements OnInit {
     public ngOnInit() {
         this.search({}); // Busqueda inicial sin parametros/filtros
     }
-    
-    search(searchParams){
+
+    search(searchParams) {
         this.searchStart(searchParams);
-        this.objectService.get(this.dataService, {...searchParams,...this.sortParams})
-        .subscribe(
-            objects => {
-                this.searchEnd(objects);
-            },
-            (err) => {
-                this.searchEnd([]);
-            }
-        );
+        this.objectService.get(this.dataService, { ...searchParams, ...this.sortParams })
+            .subscribe(
+                objects => {
+                    this.searchEnd(objects);
+                },
+                (err) => {
+                    this.searchEnd([]);
+                }
+            );
     }
 
-    protected get sortParams(){
-        if (this.sortColumn){
-            return { sort: this.sortColumn }
+    protected get sortParams() {
+        if (this.sortColumn) {
+            return { sort: this.sortColumn };
         }
-        return { };
+        return {};
     }
 
     /**
-     * Al inicializar una busqueda se preparan las variables que 
+     * Al inicializar una busqueda se preparan las variables que
      * alojaran los resultados y se actualizan las variables de
      * control que proveen feedback al usuario sobre lo que esta
      * ocurriendo.
      */
-    searchStart(searchParams){
+    searchStart(searchParams) {
         this.searchParams = searchParams;
         this.searching = true;
-        this.items = []
+        this.items = [];
         this.hiddenItems = [];
         this.showMore = false;
         this.itemSelected = null;
     }
 
-     /**
-     * Al finalizar una busqueda se actualizan las variables de control
-     * que proveen feedback al usuario indicando esta situacion y luego
-     * se delega al metodo showMoreResultados() la responsabilidad de
-     * mostrar los items y el boton de 'paginado'.
-     * @param items 
-     */
-    searchEnd(items:any){
+    /**
+    * Al finalizar una busqueda se actualizan las variables de control
+    * que proveen feedback al usuario indicando esta situacion y luego
+    * se delega al metodo showMoreResultados() la responsabilidad de
+    * mostrar los items y el boton de 'paginado'.
+    * @param items
+    */
+    searchEnd(items: any) {
         this.searching = false;
-        this.searched = true; 
-        this.hiddenItems = items ;
+        this.searched = true;
+        this.hiddenItems = items;
         this.itemSelected = null;
-        this.showMoreResultados();   
+        this.showMoreResultados();
     }
 
-    
-    public showMoreResultados(e?:any){
-        if (this.hiddenItems.length > 30){
+
+    public showMoreResultados(e?: any) {
+        if (this.hiddenItems.length > 30) {
             this.showMore = true;
-            this.items = this.items.concat(this.hiddenItems.slice(0,29));
+            this.items = this.items.concat(this.hiddenItems.slice(0, 30));
             this.hiddenItems = this.hiddenItems.slice(30);
-        }
-        else{
+        } else {
             this.showMore = false;
             this.items = this.items.concat(this.hiddenItems);
             this.hiddenItems = [];
-        }   
+        }
     }
 
 
     // ITEMS ACTIONS DEL LISTADO
 
-    public onItemHover(obj:any){
+    public onItemHover(obj: any) {
 
     }
 
-    public onItemSelectionChanged(obj:any){
+    public onItemSelectionChanged(obj: any) {
         this.itemSelected = obj;
     }
 
-    public onItemDelete(item:any){
+    public onItemDelete(item: any) {
         this.objectService.delete(this.dataService, item._id)
             .subscribe(
                 data => {
                     this.removeItemFromList(item);
                 },
-                error => {}
-            )
+                error => { }
+            );
     }
 
-    public removeItemFromList(item){
-        this.items = this.items.filter(x => x._id != item._id);
+    public removeItemFromList(item) {
+        this.items = this.items.filter(x => x._id !== item._id);
     }
 
     /**
      * Return actual url. Be careful when using with queryParams.
      * Override this method if required
      */
-    public getItemActionsBaseUrl(){
+    public getItemActionsBaseUrl() {
         return this.router.url;
     }
 
-    public onItemEdit(obj:any){
+    public onItemEdit(obj: any) {
         this.itemSelected = obj;
         this.router.navigate([this.getItemActionsBaseUrl(), 'editar', obj._id]);
     }
 
 
-    public onItemHistory(obj:any){
-        if (!this.modelName) return; // Alertar mal configurado
+    public onItemHistory(obj: any) {
+        if (!this.modelName) { return; } // Alertar mal configurado
         this.router.navigate(['/historia', this.modelName, obj._id]);
     }
 
-    public onItemAction(actionEvent: IActionEvent){
+    public onItemAction(actionEvent: IActionEvent) {
 
     }
 
 
     // GLOBAL HEADER ACTIONS
-   
-    public createItem(){
+
+    public createItem() {
         this.router.navigate([this.getItemActionsBaseUrl(), 'crear']);
     }
 
-    public cancel(){
+    public cancel() {
         this.router.navigate(['/configuracion']);
     }
 
