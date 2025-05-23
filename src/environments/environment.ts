69323d335a2d408a3e6c9785133334e4838200c3
maring0019 <57371507+maring0019@users.noreply.github.com>
2025-05-23 12:05:08 -0300

    LOGIN: se saca comentario innecesario  LOGIN-1 Y LOGIN-1A

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-//ver
+
 export const environment = {
     production: false,
     API: '//localhost:3004/api'
